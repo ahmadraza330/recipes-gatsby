e800a25b4200c0f2f0a231187835710378825354
ahmadraza330 <ra9557408@gmail.com>
2021-10-05 22:25:51 -0700

    first commit

---
+++ src/pages/company/history.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+const history = () => {
+  return (
+    <div>
+      Our history
+      <Link to="/">Home</Link>
+    </div>
+  )
+}
+
+export default history
---
+++ src/pages/contact.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const contact = () => {
+  return <div>This is contact</div>
+}
+
+export default contact
